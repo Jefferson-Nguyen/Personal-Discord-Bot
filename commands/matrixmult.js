8c03b975f0b695d738313c98461080fa1fb85420
Jefferson Nguyen <98124001+Jefferson-Nguyen@users.noreply.github.com>
2023-03-02 10:50:30 -0500

    Stuff

---
+++ commands/matrixmult.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { matrixMultiply } = require('../vendor/ualgebra');
+// const { matrix1, matrix2 } = require('../index.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName(`matrixmult`)
+		.setDescription('Multiplies 2 matrices'),
+	async execute(interaction) {
+        const m1 = global.matrix1.get(interaction.user.id);
+        const m2 = global.matrix2.get(interaction.user.id);
+        if (m1 == undefined || m2 == undefined) {
+            await interaction.reply(`${interaction.user.username} you have to set matrices using .matrix1 and .matrix2 first!`);
+            return;
+        }
+        if (m1[0].length != m2.length) {
+            await interaction.reply(`${interaction.user.username} your matrices cannot be multiplid! Remember m x n * n x p = m x p`);
+        }
+        const matrix = matrixMultiply(m1, m2);
+        await interaction.reply(`${interaction.user.username}, your matrix is: \n${matrix.join('\n')}`);
+	},
+};
---
+++ index.js
@@ -0,0 +1,97 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// Find all commands
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+// On ready
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+const matrix1 = new Map();
+const matrix2 = new Map();
+
+const parseMatrix = (input) => {
+	const data = input.split(/\r?\n/);
+	data.shift();
+	if (data.length < 1) return 'invalid';
+	const size = data[0].split(' ').length;
+	const matrix = [data.length];
+	for (let i = 0; i < data.length; i++) {
+		data[i] = data[i].split(' ');
+		if (size != data[i].length) return 'invalid';
+		matrix[i] = data[i].map((str) => parseInt(str));
+		if (isNaN(matrix[i][0])) return 'invalid';
+	}
+	return matrix;
+};
+
+client.on('messageCreate', async message => {
+	if (message.author.bot) return;
+	if (message.content.startsWith(`.hello`)) {
+		message.reply("Hello");
+	}
+	if (message.content.startsWith(`.matrix1`)) {
+		const matrix = parseMatrix(message.content);
+		if (matrix == 'invalid') {
+			message.reply('Invalid Matrix, must be rectangular');
+		}
+		matrix1.set(message.author.id, matrix);
+		message.reply(`Matrix 1 Set!`);
+	}
+	else if (message.content.startsWith(`.matrix2`)) {
+		const matrix = parseMatrix(message.content);
+		if (matrix == 'invalid') {
+			message.reply('Invalid Matrix, must be rectangular');
+		}
+		matrix2.set(message.author.id, matrix);
+		message.reply(`Matrix 2 Set!`);
+	}
+});
+global.matrix1 = matrix1;
+global.matrix2 = matrix2;
+
+
+client.login(token);
